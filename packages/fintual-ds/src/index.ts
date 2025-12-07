008e97bae70f4513fbd3765940b002d05fd3caa3
Vicente Matus <38754555+vicentematus@users.noreply.github.com>
2025-12-07 01:41:28 -0300

    chore(): update imports

--- packages/fintual-ds/src/index.ts
+++ packages/fintual-ds/src/index.ts
@@ -11,9 +11,6 @@ export { Text } from "./components/Text";
 export type { HeadingProps } from "./components/Heading";
 export type { TextProps } from "./components/Text";
 
-// Theme
-export { ThemeProvider, useTheme } from "./theme";
-
 // Design Tokens
 export { colors, spacing, typography, lightTheme, darkTheme } from "./tokens";
 export type { ColorTokens, SpacingTokens, TypographyTokens, TextColor } from "./tokens";
